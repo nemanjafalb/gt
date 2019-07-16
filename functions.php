33a5089f4450bbfc17c94afe3bd7950c3338f071
Nemanja Falb <nemus007@hotmail.com>
2019-07-16 21:25:19 +0200

    “page-homepage”

--- functions.php
+++ functions.php
@@ -148,3 +148,12 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+
+// Activate WordPress Maintenance Mode
+function wp_maintenance_mode(){
+	if(!current_user_can('edit_themes') || !is_user_logged_in()){
+	wp_die('<h1 style="color:red">Website under Maintenance</h1><br />We are performing scheduled maintenance.We will be back on-line shortly!');
+	}
+	}
+	add_action('get_header', 'wp_maintenance_mode');
---
+++ page-homepage.php
@@ -0,0 +1,22 @@
+<?php /* Template Name: Greentest */ ?>
+<header>
+	<nav>
+		<li><img src="http://localhost/gt/wp-content/uploads/2019/07/logo.jpg" alt=""></li>
+		<ul>
+			<li><a href="#">Home</a><span class="space">/</span></li>
+			<li><a href="#">Menu link</a><span class="space">/</span></li>
+			<li><a href="#">Menu link</a><span class="space">/</span></li>
+			<li><a href="#">Menu link</a><span class="space">/</span></li>
+			<li class="button"><a href="#">Button</a></li>
+		</ul>
+	</nav>
+	<section>
+		<h1>Nullam in nisl posuere</h1>
+		<p>In maximus blandit tortor, ut efficitur tortor suscipit vitae.</p>
+		<img src="/gt/wp-content/themes/gt/images/objekat.jpg" alt="slika">
+		<button>Ut sed odio quis
+			<hr>
+				Suspendisse sagittis felis
+		</button>
+	</section>
+</header>	
