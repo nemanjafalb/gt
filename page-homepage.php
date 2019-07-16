2b7aefe12ac57def6da44325006d5f4de509b295
Nemanja Falb <nemus007@hotmail.com>
2019-07-16 22:27:59 +0200

    nesto

--- page-homepage.php
+++ page-homepage.php
@@ -1,22 +1,36 @@
 <?php /* Template Name: Greentest */ ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+
+<?php wp_head(); ?>
+</head>
 <header>
 	<nav>
-		<li><img src="http://localhost/gt/wp-content/uploads/2019/07/logo.jpg" alt=""></li>
+		<img src="http://localhost/gt/wp-content/uploads/2019/07/logo.jpg" alt="">
 		<ul>
 			<li><a href="#">Home</a><span class="space">/</span></li>
-			<li><a href="#">Menu link</a><span class="space">/</span></li>
+			<li><a class="active" href="#">Menu link</a><span class="space">/</span></li>
 			<li><a href="#">Menu link</a><span class="space">/</span></li>
 			<li><a href="#">Menu link</a><span class="space">/</span></li>
 			<li class="button"><a href="#">Button</a></li>
 		</ul>
 	</nav>
-	<section>
-		<h1>Nullam in nisl posuere</h1>
-		<p>In maximus blandit tortor, ut efficitur tortor suscipit vitae.</p>
+	<section class="big-slider">
+        <div>
+            <h1>Nullam in nisl posuere</h1>
+            <p>In maximus blandit tortor, ut efficitur tortor suscipit vitae.</p>
+        </div>
+
 		<img src="/gt/wp-content/themes/gt/images/objekat.jpg" alt="slika">
 		<button>Ut sed odio quis
 			<hr>
-				Suspendisse sagittis felis1
+				Suspendisse sagittis felis
 		</button>
 	</section>
 </header>	
