18b6749c934152b023f550b5d27ed70b839974c6
Nemanja Falb <nemus007@hotmail.com>
2019-07-17 05:08:46 +0200

    “zajeb”

--- page-homepage.php
+++ page-homepage.php
@@ -1,27 +1,71 @@
 <?php
 /* Template Name: Greentest */
 ?>
-<header>
-	<nav>
-		<img src="images/logo.jpg" alt="">
-		<ul>
-			<li><a href="#">Home</a><span class="space">/</span></li>
-			<li><a class="active" href="#">Menu link</a><span class="space">/</span></li>
-			<li><a href="#">Menu link</a><span class="space">/</span></li>
-			<li><a href="#">Menu link</a><span class="space">/</span></li>
-			<li class="button"><a href="#">Button</a></li>
-		</ul>
-	</nav>
+<?php wp_head(); ?>
+    <header>
+        <nav class="container">
+            <div class="logo">
+                <img src="/gt/wp-content/themes/gt/images/logo.jpg" alt="logo">
+            </div>
+            <ul>
+                <li><a class="active" href="#">Home</a></li>
+                <li><a href="#">Menu link</a></li>
+                <li><a href="#">Menu link</a></li>
+                <li><a href="#">Menu link</a></li>
+                <li><button><a href="">Button</a></button></li>
+            </ul>
+        </nav>
+    </header>    
 	<section class="big-slider">
-        <div>
+        <div class="container">
             <h1>Nullam in nisl posuere</h1>
             <p>In maximus blandit tortor, ut efficitur tortor suscipit vitae.</p>
         </div>
+        <div class="slider">
+            <div class="left"></div>
+            <div class="right"></div>
+            <img src="/gt/wp-content/themes/gt/images/objekat.jpg" alt="slika">
+            
+            <!-- <img src="" alt="slika"> -->
+        </div>
+        <div class="buttom">
+            <button>Ut sed odio quis
+                <hr>
+                    Suspendisse sagittis felis
+            </button>
+        </div>
+    </section>
+    <section class="what-we-do">
+        <div class="container">
+            <h1>Curabitur lacus ipsum</h1>
+            <p>In maximus blandit tortor, ut efficitur tortor suscipit vitae.</p>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div class="box-three">
+            <div class="space-div">
+                <img src="/gt/wp-content/themes/gt/images/image1.jpg" alt="">
+                <span>01</span>
+                <h2>lacus</h2>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore...</p>
+            </div>
+            <div class="space-div">
+                <img src="/gt/wp-content/themes/gt/images/image1.jpg" alt="">
+                <span>01</span>
+                <h2>lacus</h2>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore...</p>
+            </div>
+            <div class="space-div">
+                <img src="/gt/wp-content/themes/gt/images/image1.jpg" alt="">
+                <span>01</span>
+                <h2>lacus</h2>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore...</p>
+            </div>
+            <div>
 
-		<img src="/gt/wp-content/themes/gt/images/objekat.jpg" alt="slika">
-		<button>Ut sed odio quis
-			<hr>
-				Suspendisse sagittis felis
-		</button>
-	</section>
-</header>
+            </div>
+            <div>
+
+            </div>
+        </div>
+        
+    </section>
